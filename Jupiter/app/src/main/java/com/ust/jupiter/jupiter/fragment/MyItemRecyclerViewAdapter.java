4166675ef434822bd542ba0c55dd788543a4e569
Sara_Hsu <lookatsirius@gmail.com>
2017-09-19 13:37:28 +0800

    Modified cu layouts

--- Jupiter/app/src/main/java/com/ust/jupiter/jupiter/fragment/MyItemRecyclerViewAdapter.java
+++ Jupiter/app/src/main/java/com/ust/jupiter/jupiter/fragment/MyItemRecyclerViewAdapter.java
@@ -6,10 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.ust.jupiter.jupiter.R;
 import com.ust.jupiter.jupiter.fragment.CUmemListFragment.OnListFragmentInteractionListener;
 import com.ust.jupiter.jupiter.fragment.dummy.DummyContent.DummyItem;
 
 import java.util.List;
+import android.R.*;
 
 /**
  * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
